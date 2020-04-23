d04549df5414e0479528be224ca3e10b4054032f
Mikael Nowak <mikael.nowak@learnet.se>
2020-04-23 12:50:31 +0200

    Jobbalite själv + skolsaker

---
+++ egen-job/rotmg/rotmg.js
@@ -0,0 +1,46 @@
+//Element
+const eCanvas = document.querySelector('canvas');
+
+//Canvas
+eCanvas.width = 1920;
+eCanvas.height = 979;
+var ctx = eCanvas.getContext("2d");
+
+//Caracter
+var knight = new Image();
+knight.src = "./photos/Knight.png";
+
+//Position
+var knightX = 750, knightY = 400;
+
+//Start
+loop();
+
+function loop() {
+    ctx.clearRect(0, 0, 1600, 900);
+    drawKnight();
+    requestAnimationFrame(loop);
+}
+
+//Draw Knight
+function drawKnight() {
+    ctx.drawImage(knight, knightX, knightY);
+}
+
+window.addEventListener("keydown", function(e) {
+    console.log(e.key);
+    switch (e.key) {
+        case 'w':
+            knightY -= 5;
+            break;
+        case 's':
+            knightY += 5;
+            break;
+        case 'd':
+            knightX += 5;
+            break;
+        case 'a':
+            knightX -= 5;
+            break;
+    }
+});
---
+++ javascript/9-brakeout/breakout.js
@@ -0,0 +1,128 @@
+//Element
+const eCanvas = document.querySelector('canvas');
+
+//Canvas
+var ctx = eCanvas.getContext('2d');
+
+/* ************************************ */
+/*                Player                */
+/* ************************************ */
+//Variable/object
+var pWidth = 200,
+pHeight = 50,
+pMarginBottom = 50;
+
+var player = {
+    x: eCanvas.width/2 - pWidth/2,
+    y: eCanvas.height - pHeight - pMarginBottom,
+    width: pWidth,
+    height: pHeight,
+    dx: 5
+}
+
+//Design
+function drawPlayer1() {
+    ctx.fillStyle = '#F8F8FF';
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+
+    ctx.strokeStyle = '#696969';
+    ctx.lineWidth = 8;
+    ctx.strokeRect(player.x, player.y, player.width, player.height);
+}
+
+//Left and right
+let left = false;
+let right = false;
+
+//Press down (move)
+document.addEventListener('keydown', function(e){
+    if (event.keyCode == 65) {
+        left = true;
+    } else if (event.keyCode == 68){
+        right = true;
+    }
+});
+
+//Not pressing (no move)
+document.addEventListener('keyup', function(e){
+    if (event.keyCode == 65) {
+        left = false;
+    } else if (event.keyCode == 68){
+        right = false;
+    }
+});
+
+//Collision detection
+function moveP1() {
+    if (right && player.x + player.width < eCanvas.width) {
+        player.x += player.dx;
+    } else if (left && player.x > 0) {
+        player.x -= player.dx;
+    }
+}
+
+/* ************************************ */
+/*                 Ball                 */
+/* ************************************ */
+//Variable/object
+var ballSize = 30;
+
+var ball = {
+    x: eCanvas.width/2,
+    y: player.y - ballSize,
+    radius: ballSize,
+    speed: 3,
+    dx: 3,
+    dy: -3
+}
+
+//Design
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+
+    ctx.fillStyle = '#F8F8FF';
+    ctx.fill();
+
+    ctx.strokeStyle = "#696969";
+    ctx.stroke();
+
+    ctx.closePath();
+}
+
+//Movement
+function moveBall() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
+
+//Collision detection
+var life = 3;
+
+if (ball.x + ball.radius > eCanvas.width || ball.x + ball.radius < 0) {
+    ball.dx = -ball.dx;
+}
+if (ball.y + ball.radius < 0) {
+    ball.dy = -ball.dy;
+}
+if (condition) {
+    
+}
+
+
+/* ************************************ */
+/*                 Loop                 */
+/* ************************************ */
+function loop() {
+    ctx.clearRect(0, 0, eCanvas.width, eCanvas.height);
+
+    drawPlayer1();
+    drawBall();
+
+    moveP1();
+    moveBall();
+
+    requestAnimationFrame(loop);
+}
+
+loop();
